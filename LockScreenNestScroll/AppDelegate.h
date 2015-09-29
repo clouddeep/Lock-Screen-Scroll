e0d3c7e57438d527456cc4bd91528dcd013834c6
clouddeep <shoujhengduan@gmail.com>
2015-09-29 10:18:21 +0800

    Initial Commit

---
+++ LockScreenNestScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LockScreenNestScroll
+//
+//  Created by Shou Cheng Tuan  on 2015/9/29.
+//  Copyright (c) 2015年 Shou Cheng Tuan . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LockScreenNestScroll/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LockScreenNestScroll
+//
+//  Created by Shou Cheng Tuan  on 2015/9/29.
+//  Copyright (c) 2015年 Shou Cheng Tuan . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
